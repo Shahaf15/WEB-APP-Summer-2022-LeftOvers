94d7470143e5fa20872a41057572344ea897f3a9
Shahaf15 <shahafpour@gmail.com>
2022-09-17 00:49:22 +0300

    final decoration

---
+++ LeftOversProject/static/GeoLocation.js
@@ -0,0 +1,22 @@
+var latitude;
+var longitude;
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition, showError);
+    } else {
+        console.log("Geolocation is not supported by this browser.");
+    }
+}
+
+function showPosition(position) {
+    latitude = position.coords.latitude;
+    longitude = position.coords.longitude;
+    console.log("lat: " + latitude + " lng: " + longitude);
+}
+
+function showError(error) {
+    if (error.code == error.PERMISSION_DENIED) {
+        alert("If you want to Help the environment and search for leftovers.\nPlease Refresh the page and allow Location");
+        console.log("Location request denied");
+    }
+}
